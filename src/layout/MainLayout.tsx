4e9fa1cb06821901e016759b762939aa6aea5866
Jihyun <jihyun@example.com>
2025-10-03 18:20:05 +0900

    기초만들기

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -4,14 +4,19 @@ import "./MainLayout.css";
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="layout">
-      <header className="navbar">🧱 구조만 있는 앱</header>
+      <header className="navbar">🧱 Rolled Egg Clinic</header>
       <div className="body">
         <aside className="sidebar">
           <nav>
             <ul>
               <li><Link to="/">홈</Link></li>
-              <li><Link to="/settings">설정</Link></li>
+              <li><Link to="/Start Playing">악기선택</Link></li>
+              <li><Link to="/settings">음악파일</Link></li>
+              <li><Link to="/settings">생활관리</Link></li>
+              <li><Link to="/settings">채팅설정</Link></li>
+              <li><Link to="/settings">옵션관리</Link></li>
               <li><Link to="/extra">기타</Link></li>
+              <li><Link to="/extra">도움요청</Link></li>
             </ul>
           </nav>
         </aside>
---
+++ src/pages/Start Playing.tsx
@@ -0,0 +1,10 @@
+export default function Start Playing() {
+  return (
+    <section>
+      <h2>⚙️악기선택</h2>
+      <p>사용자 악기 설정을 구성하는 페이지입니다.</p>
+    </section>
+  );
+}
+
+
