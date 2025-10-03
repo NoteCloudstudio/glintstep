7032b0dd05b1dc20cc3640a3353ed6ec7aec85ea
Jihyun <jihyun@example.com>
2025-10-03 18:20:05 +0900

    기초만들기

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,7 @@
 export default function Settings() {
   return (
     <section>
-      <h2>⚙️ 설정</h2>
+      <h2>⚙️옵션관리</h2>
       <p>사용자 환경 설정, 테마, 알림, 계정 관리 등을 구성하는 페이지입니다.</p>
     </section>
   );
